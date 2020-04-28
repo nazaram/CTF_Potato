0375b8e2521cf5196843ac087918e460579092a0
AramNazarian <aram.nazarian@gmail.com>
2020-04-28 11:40:19 -0700

    4.28

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -1,7 +1,40 @@
 -- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
 
 require('internal/util')
-require('gamemode')
+--require('gamemode')
+
+print("test0")
+
+BAREBONES_DEBUG_SPEW = TRUE
+
+if GameMode == nil then
+  GameMode = class({})
+end
+
+
+if score == nil then
+  score = class({})
+  score.Bad = 0
+  score.Good = 0
+end
+
+print("test1")
+if CONSTANTS == nil then
+  CONSTANTS = {}
+  CONSTANTS.scoreToWin = 10
+  CONSTANTS.goldForPoint = 150
+  CONSTANTS.goldForCatch = 15
+  CONSTANTS.goldForSave = 10
+  -- using this for scoreboard
+  -- deny is point
+  -- kill is catch
+  -- death is be caught
+  -- assist is save a friend
+end
+print("test2")
+
+
+
 
 function Precache( context )
 --[[
@@ -13,7 +46,7 @@ function Precache( context )
 
   See GameMode:PostLoadPrecache() in gamemode.lua for more information
   ]]
-
+print("test3")
   DebugPrint("[BAREBONES] Performing pre-load precache")
 
   -- Particles can be precached individually or by folder
@@ -42,10 +75,335 @@ function Precache( context )
   -- Custom units from npc_units_custom.txt can also have all of their abilities and precache{} blocks precached in this way
   PrecacheUnitByNameSync("npc_dota_hero_ancient_apparition", context)
   PrecacheUnitByNameSync("npc_dota_hero_enigma", context)
+
+  PrecacheItemByNameSync("item_capture_good_flag", context)
+  PrecacheItemByNameSync("item_capture_bad_flag", context)
+  PrecacheResource("particle", "particles/econ/courier/courier_golden_roshan/golden_roshan_ambient.vpcf", context)
+
+print("test4")
 end
+
+print("test4.5")
+
+
+
 
 -- Create the game mode when we activate
 function Activate()
   GameRules.GameMode = GameMode()
   GameRules.GameMode:_InitGameMode()
+
 end
+ 
+ function GameMode:InitGameMode()
+    print("Test7")
+
+  GameMode = GameRules:GetGameModeEntity()
+
+
+  --Override the values of the team values on the top game bar.
+  GameMode:SetTopBarTeamValuesOverride(true)
+  GameMode:SetTopBarTeamValuesVisible(true)
+  GameMode:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS, 0)
+  GameMode:SetTopBarTeamValue(DOTA_TEAM_BADGUYS, 0)
+
+
+  GameMode:SetRecommendedItemsDisabled(true)
+
+  spawnGoodFlag()
+  spawnBadFlag()
+
+  print(" Gamemode rules are set.")
+
+  ListenToGameEvent('npc_spawned', Dynamic_Wrap(GameMode, 'OnNPCSpawned'), self)
+  ListenToGameEvent('entity_hurt', Dynamic_Wrap(GameMode, 'OnEntityHurt'), self)
+
+
+  -- Register Think
+  GameRules:GetGameModeEntity():SetThink("OnThink", self, "GlobalThink", 2)
+end
+
+--[[
+  This function is called once and only once for every player when they spawn into the game for the first time.  It is also called
+  if the player's hero is replaced with a new hero for any reason.  This function is useful for initializing heroes, such as adding
+  levels, changing the starting gold, removing/adding abilities, adding physics, etc.
+  The hero parameter is the hero entity that just spawned in.
+]]
+
+function spawnGoodFlag()
+  print("entrou spawnGoodFlag")
+  local flag = CreateItem("item_capture_good_flag", nil, nil)
+  CreateItemOnPositionSync(Vector(-1822.81, -636.813, 128), flag)
+  print("saiu spawnGoodFlag")
+end
+
+function spawnBadFlag()
+  print("entrou spawnBadFlag")
+  local flag = CreateItem("item_capture_bad_flag", nil, nil)
+  CreateItemOnPositionSync(Vector(-153.281, 2337.62, 128), flag)
+  print("saiu spawnBadFlag")
+end
+
+  spawnGoodFlag()
+  spawnBadFlag()
+
+function GameMode:OnNPCSpawned(keys)
+  local hero = EntIndexToHScript(keys.entindex)
+  if hero:IsHero() then
+    print("OnNPCSpawned zerando pontos de habilidade")
+    hero:SetAbilityPoints(0)
+  end
+end
+
+function GameMode:OnThink()
+  if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+    if GameMode.playersAssigned == false then
+      GameMode:assignPlayers()
+    end
+  elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+    return nil
+  end
+  return 1
+end
+
+
+
+
+
+ 
+
+-- An entity somewhere has been hurt.
+
+function GameMode:OnEntityHurt(keys)
+  local attacker = EntIndexToHScript(keys.entindex_attacker)
+  local victim= EntIndexToHScript(keys.entindex_killed)
+
+  if attacker:IsHero() == false then
+    return
+  end
+ 
+
+
+  print("time attacker: "..attacker:GetTeamNumber())
+  print("time victim: "..victim:GetTeamNumber())
+
+  --DOTA_UNIT_CAP_MOVE_NONE
+  --DOTA_UNIT_CAP_MOVE_GROUND
+  attacker:SetMoveCapability(DOTA_UNIT_CAP_MOVE_NONE)
+  --DOTA_UNIT_CAP_NO_ATTACK
+  --DOTA_UNIT_CAP_MELEE_ATTACK
+  attacker:SetAttackCapability(DOTA_UNIT_CAP_NO_ATTACK)
+  -- create item to apply modifier
+  local itemmod = CreateItem("item_apply_modifiers", victim, victim)
+  if attacker:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
+    giveTeamGold (CONSTANTS.badGuysHero, CONSTANTS.goldForCatch)
+    victim:IncrementKills(attacker:GetPlayerID());
+    attacker:IncrementDeaths(victim:GetPlayerID());
+    itemmod:ApplyDataDrivenModifier(victim, attacker, "modifier_mute", {duration=-1})
+    -- check if there is someone of his tem unfrozen
+    local teamg = Entities:FindAllByName(CONSTANTS.goodGuysHero)
+    local allfrozen = true
+    for k,v in pairs(teamg) do
+      if v:HasGroundMovementCapability() then
+        allfrozen = false
+      end
+    end
+    if allfrozen then
+      point(CONSTANTS.badGuysHero)
+      victim:IncrementDenies()
+      return
+    end
+    attacker:SetTeam(DOTA_TEAM_CUSTOM_1)
+    print("If you have to drop an item")
+    for i = 0,5 do
+      print("i:"..i)
+      local item = attacker:GetItemInSlot(i)
+      if item then
+        print("item none:"..item:GetAbilityName())
+        if item:GetAbilityName() == "item_capture_bad_flag" then
+          attacker:RemoveItem(item)
+          print("drop item")
+          spawnBadFlag()
+        end
+      end
+    end
+  elseif attacker:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+    giveTeamGold (CONSTANTS.goodGuysHero, CONSTANTS.goldForCatch)
+    victim:IncrementKills(attacker:GetPlayerID());
+    attacker:IncrementDeaths(victim:GetPlayerID());
+    itemmod:ApplyDataDrivenModifier(victim, attacker, "modifier_mute", {duration=-1})
+    -- check if there is someone of his tem unfrozen
+    local teamb = Entities:FindAllByName(CONSTANTS.badGuysHero)
+    local allfrozen = true
+    for k,v in pairs(teamb) do
+      if v:HasGroundMovementCapability() then
+        allfrozen = false
+      end
+    end
+    if allfrozen then
+      point(CONSTANTS.goodGuysHero)
+      victim:IncrementDenies()
+      return
+    end
+    attacker:SetTeam(DOTA_TEAM_CUSTOM_2)
+    print("tem que dropar item")
+    for i = 0,5 do
+      print("i:"..i)
+      local item = attacker:GetItemInSlot(i)
+      if item then
+        print("item nome:"..item:GetAbilityName())
+        if item:GetAbilityName() == "item_capture_good_flag" then
+          attacker:RemoveItem(item)
+          print("dropando item")
+          spawnGoodFlag()
+        end
+      end
+    end
+  elseif attacker:GetTeamNumber() == DOTA_TEAM_CUSTOM_1 then
+    if victim:GetTeamNumber() == DOTA_TEAM_GOODGUYS then
+      giveTeamGold (CONSTANTS.goodGuysHero, CONSTANTS.goldForSave)
+      victim:IncrementAssists(attacker:GetPlayerID());
+      attacker:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
+      attacker:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+      attacker:SetTeam(DOTA_TEAM_GOODGUYS)
+      attacker:RemoveModifierByName("modifier_mute")
+    end
+  elseif attacker:GetTeamNumber() == DOTA_TEAM_CUSTOM_2 then
+    if victim:GetTeamNumber() == DOTA_TEAM_BADGUYS then
+      giveTeamGold (CONSTANTS.badGuysHero, CONSTANTS.goldForSave)
+      victim:IncrementAssists(attacker:GetPlayerID());
+      attacker:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
+      attacker:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+      attacker:SetTeam(DOTA_TEAM_BADGUYS)
+      attacker:RemoveModifierByName("modifier_mute")
+    end
+  end
+end
+
+function GameMode:updateScore(scoreGood, scoreBad)
+  print("Updating score: " .. scoreGood .. " x " .. scoreBad)
+
+  local GameMode = GameRules:GetGameModeEntity()
+  GameMode:SetTopBarTeamValue(DOTA_TEAM_GOODGUYS, scoreGood)
+  GameMode:SetTopBarTeamValue(DOTA_TEAM_BADGUYS, scoreBad)
+
+  -- If any team reaches scoreToWin, the game ends and that team is considered winner.
+  if scoreGood == CONSTANTS.scoreToWin then
+    print("Team GOOD GUYS victory!")
+    GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
+  end
+  if scoreBad == CONSTANTS.scoreToWin then
+    print("Team BAD GUYS victory!")
+    GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
+  end
+end
+
+
+
+-- arg is the name of the hero how made point
+-- remove flag from inventory and respawn flag if need
+-- reset and respawn heroes
+function point(nameHero)
+  if nameHero == CONSTANTS.goodGuysHero then
+    print("Radiant Point!}")
+    score.Good = score.Good + 1
+  else
+    print("Dire Point!")
+    score.Bad = score.Bad + 1
+  end
+  GameMode:updateScore(score.Good, score.Bad)
+  -- gold for the team how made point
+  giveTeamGold (nameHero, CONSTANTS.goldForPoint)
+  -- reset heroes of good guys e drop/respawn flag if need
+  local teamg = Entities:FindAllByName(CONSTANTS.goodGuysHero)
+  for k,v in pairs(teamg) do
+    for i = 0,5 do
+      local item = v:GetItemInSlot(i)
+      if item then
+        if item:GetAbilityName() == "item_capture_bad_flag" then
+          UTIL_RemoveImmediate(item)
+          spawnBadFlag()
+        end
+      end
+    end
+    v:SetTeam(DOTA_TEAM_GOODGUYS)
+    v:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
+    v:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+    v:RemoveModifierByName("modifier_mute")
+    v:SetTimeUntilRespawn(0)
+  end
+  -- reset heroes of bad guys e drop/respawn flag if need
+  local teamb = Entities:FindAllByName(CONSTANTS.badGuysHero)
+  for k,v in pairs(teamb) do
+    for i = 0,5 do
+      local item = v:GetItemInSlot(i)
+      if item then
+        if item:GetAbilityName() == "item_capture_good_flag" then
+          UTIL_RemoveImmediate(item)
+          spawnGoodFlag()
+        end
+      end
+    end
+    v:SetTeam(DOTA_TEAM_BADGUYS)
+    v:SetMoveCapability(DOTA_UNIT_CAP_MOVE_GROUND)
+    v:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+    v:RemoveModifierByName("modifier_mute")
+    v:SetTimeUntilRespawn(0)
+  end
+end
+
+function giveTeamGold (nameTeam, gold)
+  local team = Entities:FindAllByName(nameTeam)
+  for k,v in pairs(team) do
+    v:SetGold(v:GetGold() + gold, false)
+  end
+end
+
+
+--]]
+
+
+
+
+ function LevelUpMessage (eventInfo)
+     Say(nil, "Someone just leveled up!", false)
+ end
+
+ function Activate ()
+     ListenToGameEvent("dota_player_gained_level", LevelUpMessage, nil)
+ end
+
+
+ function GiveBlinkDagger (hero)
+    if hero:HasRoomForItem("item_blink", true, true) then
+       local dagger = CreateItem("item_blink", hero, hero)
+       dagger:SetPurchaseTime(0)
+       hero:AddItem(dagger)
+    end
+ end
+
+
+
+ function OnHeroPicked (event)
+    local hero = EntIndexToHScript(event.heroindex)
+    GiveBlinkDagger(hero)
+ end
+
+--[[
+function GameMode:SpawnFlag()
+        print("spawn radiant flag")
+        self.GoodguysFlagOnBase = true
+        local spawnLocation = Entities:FindByName( nil, "flag_spawn_point" )
+        local newItem = CreateItem( "item_ctf_flag", nil, nil )
+        newItem:SetPurchaseTime( 0 )
+        local drop = CreateItemOnPositionSync( spawnLocation:GetAbsOrigin(), newItem )
+end
+--]]
+
+--[[
+ function Activate ()
+    ListenToGameEvent("dota_player_pick_hero", OnHeroPicked, nil)
+ end
+--]]
+
+
--- scripts/vscripts/settings.lua
+++ scripts/vscripts/settings.lua
@@ -2,7 +2,7 @@
 
 
 ENABLE_HERO_RESPAWN = true              -- Should the heroes automatically respawn on a timer or stay dead until manually respawned
-UNIVERSAL_SHOP_MODE = false             -- Should the main shop contain Secret Shop items as well as regular items
+UNIVERSAL_SHOP_MODE = true             -- Should the main shop contain Secret Shop items as well as regular items
 ALLOW_SAME_HERO_SELECTION = true        -- Should we let people select the same hero as each other
 
 HERO_SELECTION_TIME = 30.0              -- How long should we let people select their hero?
@@ -39,7 +39,7 @@ ENABLE_TOWER_BACKDOOR_PROTECTION = false-- Should we enable backdoor protection 
 REMOVE_ILLUSIONS_ON_DEATH = false       -- Should we remove all illusions if the main hero dies?
 DISABLE_GOLD_SOUNDS = false             -- Should we disable the gold sound when players get gold?
 
-END_GAME_ON_KILLS = true                -- Should the game end after a certain number of kills?
+END_GAME_ON_KILLS = false                -- Should the game end after a certain number of kills?
 KILLS_TO_END_GAME_FOR_TEAM = 50         -- How many kills for a team should signify an end of game?
 
 USE_CUSTOM_HERO_LEVELS = true           -- Should we allow heroes to have custom levels?
